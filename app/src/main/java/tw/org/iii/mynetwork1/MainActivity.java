d9079166f2dd0b957a9a3dc289927306044aa9c2
Brad <brad@brad.tw>
2018-03-24 15:51:58 +0800

    v1

---
+++ app/src/main/java/tw/org/iii/mynetwork1/MainActivity.java
@@ -0,0 +1,64 @@
+package tw.org.iii.mynetwork1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private ConnectivityManager connectivityManager;
+    private MyNetworkBroadcast receiver;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        connectivityManager =
+                (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+        Log.v("brad", "isconnect: " + isConnectNetwork());
+        Log.v("brad", "wifi: " + isConnectWifi());
+
+        receiver = new MyNetworkBroadcast();
+        IntentFilter filter = new IntentFilter(
+                ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(receiver, filter);
+
+    }
+
+    @Override
+    public void finish() {
+        if (receiver != null){
+            unregisterReceiver(receiver);
+        }
+        super.finish();
+    }
+
+    private boolean isConnectNetwork(){
+        NetworkInfo info = connectivityManager.getActiveNetworkInfo();
+        boolean isConnected = info != null && info.isConnected();
+        return isConnected;
+    }
+
+    private boolean isConnectWifi(){
+        NetworkInfo info =
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        return info.isConnected();
+    }
+
+    private class MyNetworkBroadcast extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.v("brad", "OK");
+        }
+    }
+
+
+}
